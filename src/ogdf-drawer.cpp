8f1c992214b0128cfd0a2f6bf7fb258ab5c0405a
friscobuffo <gio.andreola@stud.uniroma3.it>
2026-02-12 18:25:30 +0100

    commit

--- src/ogdf-drawer.cpp
+++ src/ogdf-drawer.cpp
@@ -17,7 +17,9 @@
 #include <chrono>
 #include <cmath>
 #include <domus/core/utils.hpp>
+#include <domus/orthogonal/equivalence_classes.hpp>
 #include <domus/orthogonal/shape/shape.hpp>
+#include <functional>
 #include <map>
 #include <string>
 
@@ -64,13 +66,14 @@ Shape compute_shape(
     return shape;
 }
 
-void compute_augmented_graph_and_positions(
+std::tuple<UndirectedGraph, std::unordered_map<int, std::pair<int, int>>>
+compute_augmented_graph_and_ogdf_positions(
     const UndirectedGraph& graph,
     const ogdf::GraphAttributes& GA,
     const ogdf::Graph& G,
-    std::unordered_map<int, int>& ogdf_index_to_nodeid,
-    UndirectedGraph& augmented_graph,
-    std::unordered_map<int, std::pair<int, int>>& id_to_ogdf_positions) {
+    std::unordered_map<int, int>& ogdf_index_to_nodeid) {
+    UndirectedGraph augmented_graph;
+    std::unordered_map<int, std::pair<int, int>> id_to_ogdf_positions;
     for (const int node_id : graph.get_nodes_ids())
         augmented_graph.add_node(node_id);
     for (ogdf::node v : G.nodes) {
@@ -100,80 +103,133 @@ void compute_augmented_graph_and_positions(
             augmented_graph.add_edge(from_id, to_id);
         }
     }
+    return std::make_tuple(augmented_graph, id_to_ogdf_positions);
 }
 
-auto compute_coordinate(
-    const std::map<int, std::vector<int>>& coordinate_to_ids) {
+std::unordered_map<int, int> compute_grid_positions(
+    const DirectedGraph& ordering_of_classes,
+    const EquivalenceClasses& equivalence_classes,
+    const std::unordered_map<int, std::pair<int, int>>& id_to_ogdf_positions,
+    std::function<int(int, const std::unordered_map<int, std::pair<int, int>>&)>
+        get_position) {
+    std::unordered_map<int, int> node_id_to_position;
+    int current_position = 0;  // position at this point are 0, 1, 2, ...
     constexpr int THRESHOLD = 4500;
-    int last_ogdf_coordinate = 0;
-    int coordinate_to_use = 0;
-    std::unordered_map<int, int> new_positions_coordinate;
-    for (const auto& [coordinate, ids] : coordinate_to_ids) {
-        if (coordinate - last_ogdf_coordinate > THRESHOLD)
-            coordinate_to_use += grid;
-        for (const int id : ids)
-            new_positions_coordinate[id] = coordinate_to_use;
-        last_ogdf_coordinate = coordinate;
+    std::unordered_map<int, int> in_degree;
+    for (int class_id : ordering_of_classes.get_nodes_ids()) {
+        for (int neighbor_class_id :
+             ordering_of_classes.get_out_neighbors_of_node(class_id)) {
+            if (!in_degree.contains(neighbor_class_id))
+                in_degree[neighbor_class_id] = 0;
+            in_degree[neighbor_class_id]++;
+        }
     }
-    return new_positions_coordinate;
-}
-
-GraphAttributes compute_graph_attributes(
-    const UndirectedGraph& graph,
-    UndirectedGraph& augmented_graph,
-    std::unordered_map<int, std::pair<int, int>>& id_to_ogdf_positions) {
-    std::map<int, std::vector<int>> x_coor_to_ids, y_coor_to_ids;
-    for (const int node_id : augmented_graph.get_nodes_ids()) {
-        const int x = id_to_ogdf_positions[node_id].first;
-        const int y = id_to_ogdf_positions[node_id].second;
-        x_coor_to_ids[x].push_back(node_id);
-        y_coor_to_ids[y].push_back(node_id);
+    std::unordered_set<int> queue;
+    for (int class_id : ordering_of_classes.get_nodes_ids())
+        if (in_degree[class_id] == 0)
+            queue.insert(class_id);
+    while (!queue.empty()) {
+        std::unordered_map<int, int> avg_coordinate_of_class;
+        for (int class_id : queue) {
+            int position_sum = 0;
+            for (int node_id : equivalence_classes.get_elems_of_class(class_id))
+                position_sum += get_position(node_id, id_to_ogdf_positions);
+            avg_coordinate_of_class[class_id] =
+                position_sum /
+                equivalence_classes.get_elems_of_class(class_id).size();
+        }
+        std::vector<int> classes(queue.begin(), queue.end());
+        std::sort(
+            classes.begin(),
+            classes.end(),
+            [&avg_coordinate_of_class](int a, int b) {
+                return avg_coordinate_of_class[a] < avg_coordinate_of_class[b];
+            });
+        for (int i = 0; i < classes.size(); ++i) {
+            if (avg_coordinate_of_class[classes[i]] -
+                    avg_coordinate_of_class[classes[0]] <
+                THRESHOLD) {
+                queue.erase(classes[i]);
+                for (int node_id :
+                     equivalence_classes.get_elems_of_class(classes[i]))
+                    node_id_to_position[node_id] = current_position;
+                for (int class_id :
+                     ordering_of_classes.get_out_neighbors_of_node(
+                         classes[i])) {
+                    if (--in_degree[class_id] == 0)
+                        queue.insert(class_id);
+                }
+            }
+        }
+        ++current_position;
     }
-    auto new_positions_x = compute_coordinate(x_coor_to_ids);
-    auto new_positions_y = compute_coordinate(y_coor_to_ids);
-    GraphAttributes attributes;
-    attributes.add_attribute(Attribute::NODES_POSITION);
-    attributes.add_attribute(Attribute::NODES_COLOR);
-    for (const int node_id : augmented_graph.get_nodes_ids()) {
-        attributes.set_position(
-            node_id, new_positions_x[node_id], new_positions_y[node_id]);
-        if (graph.has_node(node_id))  // is a true node
-            attributes.set_node_color(node_id, Color::BLACK);
-        else  // is a bend
-            attributes.set_node_color(node_id, Color::RED);
-    }
-    return attributes;
+    return node_id_to_position;
 }
 
 void make_shifts_overlapping_edges(
     UndirectedGraph& augmented_graph,
-    const GraphAttributes& attributes,
-    Shape& shape) {
-    // TODO
-    // std::map<int, std::vector<int>> x_coor_to_ids;
-    // std::map<int, std::vector<int>> y_coor_to_ids;
-    // for (const int node_id : augmented_graph.get_nodes_ids()) {
-    //     const int x = attributes.get_position_x(node_id);
-    //     const int y = attributes.get_position_y(node_id);
-    //     x_coor_to_ids[x].push_back(node_id);
-    //     y_coor_to_ids[y].push_back(node_id);
-    // }
-    // for (const auto& [x, nodes_ids] : x_coor_to_ids) {
-    //     std::sort(
-    //         nodes_ids.begin(),
-    //         nodes_ids.end(),
-    //         [&attributes](int node_1_id, int node_2_id) {
-    //             return attributes.get_position_y(node_1_id) <
-    //                    attributes.get_position_y(node_2_id);
-    //         });
-    //     std::unordered_map<int, std::vector<int>>
-    //     order_of_nodes_per_x_offset; for (const node_id : nodes_ids) {
-    //         if (attributes.get_node_color(node_id) == Color::BLACK)
-    //             order_of_nodes_per_x_offset[0].push_back(node_id);
-    //         else {
-    //         }
-    //     }
-    // }
+    GraphAttributes& attributes,
+    Shape& shape,
+    const std::unordered_map<int, std::pair<int, int>>& id_to_ogdf_positions) {
+    std::map<int, std::vector<int>> x_coor_to_ids;
+    std::map<int, std::vector<int>> y_coor_to_ids;
+    for (int node_id : augmented_graph.get_nodes_ids()) {
+        int x = attributes.get_position_x(node_id);
+        int y = attributes.get_position_y(node_id);
+        x_coor_to_ids[x].push_back(node_id);
+        y_coor_to_ids[y].push_back(node_id);
+    }
+    // TODOOOOO
+}
+
+GraphAttributes compute_graph_attributes(
+    const UndirectedGraph& graph,
+    Shape& shape,
+    UndirectedGraph& augmented_graph,
+    std::unordered_map<int, std::pair<int, int>>& id_to_ogdf_positions) {
+    auto [equivalence_x, equivalence_y] =
+        build_equivalence_classes(shape, augmented_graph);
+    auto
+        [ordering_x,
+         ordering_y,
+         ordering_x_edge_to_graph_edge,
+         ordering_y_edge_to_graph_edge] =
+            equivalence_classes_to_ordering(
+                equivalence_x, equivalence_y, augmented_graph, shape);
+    auto node_id_to_position_x = compute_grid_positions(
+        ordering_x,
+        equivalence_x,
+        id_to_ogdf_positions,
+        [](int id,
+           const std::unordered_map<int, std::pair<int, int>>&
+               id_to_ogdf_positions) {
+            return id_to_ogdf_positions.at(id).first;
+        });
+    auto node_id_to_position_y = compute_grid_positions(
+        ordering_y,
+        equivalence_y,
+        id_to_ogdf_positions,
+        [](int id,
+           const std::unordered_map<int, std::pair<int, int>>&
+               id_to_ogdf_positions) {
+            return id_to_ogdf_positions.at(id).second;
+        });
+    GraphAttributes attributes;
+    attributes.add_attribute(Attribute::NODES_POSITION);
+    attributes.add_attribute(Attribute::NODES_COLOR);
+    for (int node_id : augmented_graph.get_nodes_ids()) {
+        attributes.set_position(
+            node_id,
+            100 * node_id_to_position_x[node_id],
+            100 * node_id_to_position_y[node_id]);
+        if (graph.has_node(node_id))
+            attributes.set_node_color(node_id, Color::BLACK);
+        else
+            attributes.set_node_color(node_id, Color::RED);
+    }
+    make_shifts_overlapping_edges(
+        augmented_graph, attributes, shape, id_to_ogdf_positions);
+    return attributes;
 }
 
 OrthogonalDrawing convert_ogdf_result(
@@ -181,26 +237,37 @@ OrthogonalDrawing convert_ogdf_result(
     const ogdf::Graph& G,
     const UndirectedGraph& graph,
     std::unordered_map<int, int>& ogdf_index_to_nodeid) {
-    UndirectedGraph augmented_graph;
-    std::unordered_map<int, std::pair<int, int>> id_to_ogdf_positions;
-    compute_augmented_graph_and_positions(
-        graph,
-        GA,
-        G,
-        ogdf_index_to_nodeid,
-        augmented_graph,
-        id_to_ogdf_positions);
+    auto [augmented_graph, id_to_ogdf_positions] =
+        compute_augmented_graph_and_ogdf_positions(
+            graph, GA, G, ogdf_index_to_nodeid);
     Shape shape = compute_shape(augmented_graph, id_to_ogdf_positions);
-    GraphAttributes attributes =
-        compute_graph_attributes(graph, augmented_graph, id_to_ogdf_positions);
-    make_shifts_overlapping_edges(augmented_graph, attributes, shape);
+    GraphAttributes attributes = compute_graph_attributes(
+        graph, shape, augmented_graph, id_to_ogdf_positions);
     return {
         std::move(augmented_graph), std::move(attributes), std::move(shape)};
 }
 
+struct OgdfDrawingOutput {
+    ogdf::Graph G;
+    ogdf::GraphAttributes GA;
+    double time;
+    std::unordered_map<int, int> ogdf_index_to_nodeid;
+};
+
+OgdfDrawingOutput compute_drawing_with_ogdf(const UndirectedGraph& graph);
+
 std::pair<OrthogonalDrawing, double> make_orthogonal_drawing_ogdf(
     const UndirectedGraph& graph,
     const std::string& svg_output_filename) {
+    auto [G, GA, elapsed, ogdf_index_to_nodeid] =
+        compute_drawing_with_ogdf(graph);
+    ogdf::GraphIO::write(GA, svg_output_filename, ogdf::GraphIO::drawSVG);
+    OrthogonalDrawing result =
+        convert_ogdf_result(GA, G, graph, ogdf_index_to_nodeid);
+    return std::make_pair(std::move(result), elapsed);
+}
+
+OgdfDrawingOutput compute_drawing_with_ogdf(const UndirectedGraph& graph) {
     ogdf::Graph G;
     ogdf::GraphAttributes GA(
         G,
@@ -252,9 +319,5 @@ std::pair<OrthogonalDrawing, double> make_orthogonal_drawing_ogdf(
 
     auto end = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> elapsed = end - start;
-
-    ogdf::GraphIO::write(GA, svg_output_filename, ogdf::GraphIO::drawSVG);
-    OrthogonalDrawing result =
-        convert_ogdf_result(GA, G, graph, ogdf_index_to_nodeid);
-    return std::make_pair(std::move(result), elapsed.count());
+    return {G, GA, elapsed.count(), ogdf_index_to_nodeid};
 }
